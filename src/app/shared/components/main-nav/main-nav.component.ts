f794908af49a745969832f23abe7b42baf7065ae
Kartheek Sarabu <krthk.2005@gmail.com>
2020-07-25 12:55:23 +0100

    added dialog
    added dialog

--- src/app/shared/components/main-nav/main-nav.component.ts
+++ src/app/shared/components/main-nav/main-nav.component.ts
@@ -1,14 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { NewMeetingComponent } from '../new-meeting/new-meeting.component';
 
 @Component({
   selector: 'app-main-nav',
   templateUrl: './main-nav.component.html',
   styleUrls: ['./main-nav.component.scss']
 })
-export class MainNavComponent {
+export class MainNavComponent implements OnInit{
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
@@ -16,6 +18,14 @@ export class MainNavComponent {
       shareReplay()
     );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(private breakpointObserver: BreakpointObserver, private matDialog: MatDialog) {}
 
+  ngOnInit(): void {
+    
+  }
+
+  createMeeting() {
+    const dialogConfig = new MatDialogConfig();
+    this.matDialog.open(NewMeetingComponent, dialogConfig);
+  }
 }
